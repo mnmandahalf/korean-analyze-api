6f75271e1f544b3f7de7958f9120e66387e3fcb8
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-13 21:09:15 +0900

    corsの設定修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4000'
+    origins Rails.application.config.x.cors_allowed_origins
 
     resource '*',
       headers: :any,
